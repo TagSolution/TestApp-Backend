8b18d8458b75fdf722d707c32ba3eb74216f1e78
Tomas Padilla <padillatomasagustin@gmail.com>
2023-01-11 22:00:14 +0100

    main - allowed Origins

--- src/main/java/com/tagsolutions/TestSpringBootApp/controller/TestController.java
+++ src/main/java/com/tagsolutions/TestSpringBootApp/controller/TestController.java
@@ -1,11 +1,9 @@
 package com.tagsolutions.TestSpringBootApp.controller;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = {"http://localhost:3000", "https://tagsolution.github.io/TestApp-Frontend"})
 @RestController
 @RequestMapping("/test")
 public class TestController {
