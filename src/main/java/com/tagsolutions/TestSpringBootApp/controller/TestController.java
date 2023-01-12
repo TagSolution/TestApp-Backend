68e1ebad91de6203187a615f1e15d5defcbf52ba
Tomas Padilla <padillatomasagustin@gmail.com>
2023-01-12 11:08:17 +0100

    main - cross origins, remove dockerifle

--- src/main/java/com/tagsolutions/TestSpringBootApp/controller/TestController.java
+++ src/main/java/com/tagsolutions/TestSpringBootApp/controller/TestController.java
@@ -3,7 +3,7 @@ package com.tagsolutions.TestSpringBootApp.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = {"http://localhost:3000", "https://tagsolution.github.io/TestApp-Frontend"})
+@CrossOrigin(origins = {"http://localhost:3000", "https://tagsolution.github.io/TestApp-Frontend", "*"})
 @RestController
 @RequestMapping("/test")
 public class TestController {
@@ -11,6 +11,6 @@ public class TestController {
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/hola")
     public String getTestHola() {
-        return "Hola desde EC2!!!";
+        return "Hola desde donde sea deployado!!!!!!!!!";
     }
 }
